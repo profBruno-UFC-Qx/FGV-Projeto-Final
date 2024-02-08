028b3a3f384fa92f03124f0b356583ad41453e49
Bruno Mateus <brunomateus@gmail.com>
2024-02-07 23:28:12 -0300

    Assegurando que as rotas necessárias estarão abertas

--- TodoBack/src/index.js
+++ TodoBack/src/index.js
@@ -1,4 +1,43 @@
 'use strict';
+const PUBLIC_ROLE = "PUBLIC"
+
+async function grantRoutePermissions() {
+
+  const roles = await strapi.service("plugin::users-permissions.role").find()
+
+  async function grantPermission(systemRole, apiToGrant) {
+
+      const roleId = roles.find(role => role.type === systemRole.toLocaleLowerCase()).id
+      const role = await strapi.service("plugin::users-permissions.role").findOne(roleId)
+
+      for (let permission in apiToGrant) {
+          const controllers = role.permissions[permission].controllers
+          for (let controller in apiToGrant[permission]) {
+              for (let endPoint of apiToGrant[permission][controller]) {
+                  controllers[controller][endPoint].enabled = true
+              }
+          }
+      }
+      await strapi.service("plugin::users-permissions.role").updateRole(role.id, role);
+  }
+
+
+  async function grantPermissionsToAll() {
+
+    const apiToGrant = {
+      'api::tarefa': {
+          'tarefa': ["find", "findOne", "create", "update", "delete"]
+      },
+      'api::categoria': {
+          'categoria': ['find']
+      },
+    }
+
+    await grantPermission(PUBLIC_ROLE, apiToGrant)
+  }
+
+  await grantPermissionsToAll()
+}
 
 module.exports = {
   /**
@@ -16,5 +55,7 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap(/*{ strapi }*/) {
+    grantRoutePermissions()
+  },
 };
